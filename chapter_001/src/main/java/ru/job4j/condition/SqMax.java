4646c325c879109a28a19494649f9565b32f92e2
olliestyle <olliestyle1989@gmail.com>
2020-01-21 00:52:35 +0300

    4.5 Отладка программы в Idea

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class SqMax {
+    public static int max(int first, int second, int third, int forth) {
+        int result = forth;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
+                    result = first; // error - was result = third;
+                }
+            }
+        } else if (second > third) {
+            if (second > forth) {
+                result = second; // error - was result = first;
+            }
+        } else if (third > forth) {
+            result = third; // error - was result = second;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+
+public class SqMaxTest {
+
+    @Test
+    public void whenFirstMax(){
+        int result = SqMax.max(4,3,2,1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenSecondMax(){
+        int result = SqMax.max(3,4,2,1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenThirdMax(){
+        int result = SqMax.max(2,3,4,1);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void whenFourthMax(){
+        int result = SqMax.max(1,2,3,4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    public void allEquals(){
+        int result = SqMax.max(3,3,3,3);
+        assertThat(result, is(3));
+    }
+}
