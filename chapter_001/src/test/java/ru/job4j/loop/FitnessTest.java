6d8b45361ac377aa011663fde21fe737c64970e8
olliestyle <olliestyle1989@gmail.com>
2020-01-21 02:24:46 +0300

    5.4 Протеиновая диета

---
+++ chapter_001/src/test/java/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+
+    @Test
+    public void whenIvanLessByFourNik(){
+        Fitness fitness = new Fitness();
+        int month = fitness.calc(30, 120);
+        assertThat(month, is(4));
+    }
+
+    @Test
+    public void whenIvanGreatNik() {
+        Fitness fit = new Fitness();
+        int month = fit.calc(95, 90);
+        assertThat(month, is(0));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        Fitness fit = new Fitness();
+        int month = fit.calc(90, 95);
+        assertThat(month, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        Fitness fit = new Fitness();
+        int month = fit.calc(50, 90);
+        assertThat(month, is(2));
+    }
+
+}
