0aae1e7b0a967cc0416e70f5f860e6b415e37f15
olliestyle <olliestyle1989@gmail.com>
2020-01-23 00:58:18 +0300

    5.8 JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Calculator решение задачи части 001 урок1
+ *
+ * @author Baibolatov Murat (olliestyle1989@gmail.com)
+ */
+
 public class Calculator {
 
     public static void add(double first, double second){
@@ -22,6 +28,10 @@ public class Calculator {
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Main.
+     * @param args
+     */
     public static void main(String[] args) {
         add(1,1);
         div(4,2);
