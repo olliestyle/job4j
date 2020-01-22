8b4c30e8acb58be00fa967f81b419d3e774c209f
olliestyle <olliestyle1989@gmail.com>
2020-01-23 00:35:06 +0300

    5.7 Ипотека

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(int amount, int salary, double percent){
+
+        int year = 0;
+        int remains = 1;
+        while(remains > 0){
+            remains = (int) (amount + amount * percent/100 - salary);
+            amount = remains;
+            year++;
+        }
+        return year;
+    }
+
+}
