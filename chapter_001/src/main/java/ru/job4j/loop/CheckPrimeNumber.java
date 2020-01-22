3ef9be3ef29629ada729962aedc53af222605805
olliestyle <olliestyle1989@gmail.com>
2020-01-22 23:51:09 +0300

    5.5 Простое число

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int finish){
+
+        boolean prime = true;
+
+        for (int i = 2 ; i <= finish/2; i++){
+            if (finish % i == 0){
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CheckPrimeNumberTest {
+
+    @Test
+    public void when5(){
+        boolean rsl = CheckPrimeNumber.check(5);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when4(){
+        boolean rsl = CheckPrimeNumber.check(4);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when3571(){
+        boolean rsl = CheckPrimeNumber.check(3571);
+        assertThat(rsl, is(true));
+    }
+
+    @Test
+    public void when121(){
+        boolean rsl = CheckPrimeNumber.check(121);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when81(){
+        boolean rsl = CheckPrimeNumber.check(81);
+        assertThat(rsl, is(false));
+    }
+
+    @Test
+    public void when169(){
+        boolean rsl = CheckPrimeNumber.check(169);
+        assertThat(rsl, is(false));
+    }
+}
