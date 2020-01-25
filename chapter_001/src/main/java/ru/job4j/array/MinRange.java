24880ff56ccbf44c8dd188510a931564fe539289
olliestyle <olliestyle1989@gmail.com>
2020-01-26 02:58:21 +0300

    6.5.2 Поиск минимума в диапазоне. Попытка 2

--- chapter_001/src/main/java/ru/job4j/array/MinRange.java
+++ chapter_001/src/main/java/ru/job4j/array/MinRange.java
@@ -6,8 +6,11 @@ public class MinRange {
 
     public static int findMin(int[] toFind, int start, int finish){
         int min = toFind[start];
+        if (finish == toFind.length-1){
+            finish = toFind.length;
+        }
 
-        for (int i = start; i <= finish; i++){
+        for (int i = start; i < finish; i++){
             if(toFind[i] < min){
                 min = toFind[i];
             }
