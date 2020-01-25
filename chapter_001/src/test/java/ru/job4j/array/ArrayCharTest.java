a1e6501ecb97895344dd87f26a03225192596861
olliestyle <olliestyle1989@gmail.com>
2020-01-25 23:58:01 +0300

    6.4 Слово начинается с...

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayCharTest {
+
+    @Test
+    public void whenStartsWithPrefixTrue(){
+        char[] word = {'h','e','l','l','o'};
+        char[] pref = {'h','e'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenStartsWithPrefixFalse(){
+        char[] word = {'h','e','l','l','o'};
+        char[] pref = {'h','i'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(false));
+    }
+
+    @Test
+    public void whenStartsWithPrefixTrue1(){
+        char[] word = {'h','e','l','l','o','w','o','r','l','d'};
+        char[] pref = {'h','e','l','l','o','w','o','r','l'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenStartsWithPrefixFalse1(){
+        char[] word = {'h','e','l','l','o'};
+        char[] pref = {'h','E','l'};
+        boolean result = ArrayChar.startsWith(word, pref);
+        assertThat(result, is(false));
+    }
+}
