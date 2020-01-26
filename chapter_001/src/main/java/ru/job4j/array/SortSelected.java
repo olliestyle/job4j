87b83185e9229bf46fe70fcff75d6e5f057d57c4
olliestyle <olliestyle1989@gmail.com>
2020-01-26 03:07:02 +0300

    6.5.3 Сортировка выборкой

---
+++ chapter_001/src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data){
+
+        for (int i = 0; i < data.length; i++) {
+            int temp = data[i];
+            data[i] = MinRange.findMin(data,i,data.length);
+            data[FindLoop.indexOf(data, data[i], i, data.length)] = temp;
+        }
+
+        return data;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SortSelectedTest {
+
+    @Test
+    public void whenSortFive() {
+        int[] input = new int[] {3, 4, 1, 2, 5};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortThree() {
+        int[] input = new int[] {3, 4, 1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 3, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortTen() {
+        int[] input = new int[] {3,4,1,0,2,8,7,5,9,6};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {0,1,2,3,4,5,6,7,8,9};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortTwo() {
+        int[] input = new int[] {4, 1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1, 4};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSortOne() {
+        int[] input = new int[] {1};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {1};
+        assertThat(result, is(expect));
+    }
+}
