f035c5767d53aeb9fb70b1625d3dac05dc2c96be
olliestyle <olliestyle1989@gmail.com>
2020-02-05 00:25:47 +0300

    Not for verification

--- chapter_002/src/main/java/ru/job4j/oop/Student.java
+++ chapter_002/src/main/java/ru/job4j/oop/Student.java
@@ -2,21 +2,17 @@ package ru.job4j.oop;
 
 public class Student {
 
-    public void music(){
-        System.out.println("Tra tra tra");
+    public void music(String lyrics) {
+        System.out.println("I can sing a song : " + lyrics);
     }
 
-    public void song(){
+    public void song() {
         System.out.println("I believe i can fly");
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.song();
-        petya.song();
-        petya.song();
+        String song = "I believe, i can fly";
+        petya.music(song);
     }
 }
