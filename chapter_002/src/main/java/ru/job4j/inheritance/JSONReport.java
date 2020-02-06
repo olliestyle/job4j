67c794459a43f49b88aa918f02eaac720651fb53
olliestyle <olliestyle1989@gmail.com>
2020-02-07 01:28:48 +0300

    4. Переопределение[#222449]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,7 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{\n\n name : " + name + ",\n" + " body : " + body + " \n\n}.";
+    }
+}
