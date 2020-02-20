5b4c151ffff28dbf0b226d91d1c6ca5e258a69ff
olliestyle <olliestyle1989@gmail.com>
2020-02-21 01:23:47 +0300

    9. Написать тесты на StartUI.[#222417]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -3,46 +3,23 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
-    @Test
-    public void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new StubInput(answers);
-        Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName(), is(expected.getName()));
-    }
 
+    /**
+     * В StubInput input записываем ответ, который якобы считываем с клавиатуры.
+     * Инициализиуруем StubAction action.
+     * Передаем в качестве аргументов:
+     * 1. input, который якобы введет "0" в консоль при вызове метода input.askInt в методе init
+     * 2. new UserAction[] { action }, который будет указывать на объект StubAction под индексом "0"
+     * После чего выполнится метод execute() объекта action класса StubAction и call присвоится true
+     */
     @Test
-    public void whenReplaceItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(),
-                "replaced item"
-        };
-        StartUI.editItem(new StubInput(answers), tracker);
-        Item replaced = tracker.findById(item.getId());
-        assertThat(replaced.getName(), is("replaced item"));
-    }
-
-    @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {
-                item.getId(),
-                item.getName()
-        };
-        StartUI.deleteItem(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertThat(deleted, is(nullValue()));
+    public void whenExit() {
+        StubInput input = new StubInput(new String[] {"0"});
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
     }
 }
