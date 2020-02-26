fa039ec93d47b507e352b4d49cb855aa8c9929fd
olliestyle <olliestyle1989@gmail.com>
2020-02-27 02:58:58 +0300

    1. Обеспечить бесперебойную работу приложения Tracker.[#222423]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -6,7 +6,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu(actions);
-            int select = input.askInt("Select: ");
+            int select = input.askInt("Select: ", actions.length);
             UserAction action = actions[select];
             run = action.execute(input, tracker);
         }
@@ -20,10 +20,10 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input input = new ConsoleInput();
+        Input validate = new ValidateInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {new CreateAction(), new ShowAllAction(), new EditAction(), new DeleteAction(),
         new FindByIDAction(), new FindByNameAction(), new ExitAction()};
-        new StartUI().init(input, tracker, actions);
+        new StartUI().init(validate, tracker, actions);
     }
 }
