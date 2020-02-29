953a4fae3ecd5738f97bc2457457e3f1fecc685d
olliestyle <olliestyle1989@gmail.com>
2020-02-29 21:30:40 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#222421]. Попытка 2

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -24,5 +24,19 @@ public class ValidateInputTest {
         System.setOut(out);
     }
 
-
+    @Test
+    public void whenKeyNotFromMenu() {
+        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+        PrintStream out = System.out;
+        System.setOut(new PrintStream(mem));
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"8", "0"})
+        );
+        input.askInt("Enter", 6);
+        assertThat(
+                mem.toString(),
+                is(String.format("Please select key from menu.%n"))
+        );
+        System.setOut(out);
+    }
 }
