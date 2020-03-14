8297c8f21802bf94688a32cfaaef4381da199da7
olliestyle <olliestyle1989@gmail.com>
2020-03-15 00:29:15 +0300

    3. Конвертация ArrayList в двухмерный массив[#222374]

---
+++ chapter_003/src/main/java/ru/job4j/collection/ConvertList2Array.java
@@ -0,0 +1,35 @@
+package ru.job4j.collection;
+
+
+import java.util.List;
+
+public class ConvertList2Array {
+    public static int[][] toArray(List<Integer> list, int cells) {
+        int groups = (int) Math.ceil((double) list.size() / cells);
+        System.out.println(groups);
+        int[][] array = new int[groups][cells];
+        int row = 0, cell = 0;
+        for(Integer num: list) {
+            if(cell == cells) {
+                row++;
+                if(row == groups)
+                    break;
+                cell = 0;
+            }
+            array[row][cell] = num;
+            cell++;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18);
+        int[][] rsl = toArray(list, 8);
+        for (int[] row : rsl) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/search/ConvertList2ArrayTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+import ru.job4j.collection.ConvertList2Array;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ConvertList2ArrayTest {
+    @Test
+    public void when7ElementsThen9() {
+        int[][] result = ConvertList2Array.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                3
+        );
+        int[][] expect = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void when18ElementsThen20() {
+        int[][] result = ConvertList2Array.toArray(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17),
+                4
+        );
+        int[][] expect = {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16},
+                {17, 0, 0, 0}
+        };
+        assertThat(result, is(expect));
+    }
+}
