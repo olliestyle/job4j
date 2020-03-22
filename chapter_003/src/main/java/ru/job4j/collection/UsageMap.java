b1c8e7ff31b04793414210c4a3458ed77ee9ff11
olliestyle <olliestyle1989@gmail.com>
2020-03-22 14:10:10 +0300

    0. Map, HashMap[#222386]

---
+++ chapter_003/src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+
+    public static void main(String[] args) {
+        HashMap<String, String> simpleMap = new HashMap<>();
+        simpleMap.put("olliestyle1989@gmail.com", "Baibolatov Murat");
+        simpleMap.put("egor777@gmail.com", "Ivanov Egor");
+        simpleMap.put("oleg666@gmail.com", "Genkin Oleg");
+
+        System.out.println("Через метод keySet()");
+        for(String key: simpleMap.keySet()) {
+            System.out.println("key is - " + key + ", value is - " + simpleMap.get(key));
+        }
+
+        System.out.println("Через метод entrySet()");
+        for(Map.Entry<String, String> map: simpleMap.entrySet()) {
+            System.out.println("key is - " + map.getKey() + ", value is - " + map.getValue());
+        }
+    }
+}
