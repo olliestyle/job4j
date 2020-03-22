4df930353412b2e130c6e88cd485de8723d0b89d
olliestyle <olliestyle1989@gmail.com>
2020-03-22 18:32:17 +0300

    1. Паспорт и жители.[#222387]. Попытка 2

--- chapter_003/src/main/java/ru/job4j/collection/PassportOffice.java
+++ chapter_003/src/main/java/ru/job4j/collection/PassportOffice.java
@@ -8,8 +8,7 @@ public class PassportOffice {
 
     public boolean add(Citizen citizen) {
         boolean rsl = false;
-        if (citizens.containsKey(citizen.getPassport())) {
-        } else {
+        if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
             rsl = true;
         }
