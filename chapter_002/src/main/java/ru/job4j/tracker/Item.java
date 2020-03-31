4380e6ea1a8c91e2f4a7deec81c99cd681ad9bce
olliestyle <olliestyle1989@gmail.com>
2020-03-31 14:21:34 +0300

    0. Сортировка[#222365]

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class Item {
+public class Item implements Comparable<Item> {
     private String id;
     private String name;
 
@@ -22,5 +22,10 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        return id.compareTo(o.id);
     }
 }
