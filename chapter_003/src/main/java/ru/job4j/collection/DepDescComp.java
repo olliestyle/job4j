d1e91a9689850b4233d24f9e3e1fa9fe253fbe73
olliestyle <olliestyle1989@gmail.com>
2020-04-15 02:59:54 +0300

    Отсортировать департаменты[#222368]. Попытка 2

--- chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
+++ chapter_003/src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,18 +5,15 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int result = Integer.compare(o1.length(), o2.length());
+        int result = 0;
         int lengthOfMinArray = Math.min(o1.length(), o2.length());
         for (int i = 0; i < lengthOfMinArray; i++) {
-             if (o1.charAt(i) > o2.charAt(i)) {
-                result = -1;
+            if (o1.charAt(i) == '/' || o2.charAt(i) == '/') {
                 break;
-            } else if (o1.charAt(i) < o2.charAt(i)) {
-                 result = 1;
-                 break;
-             }
+            }
+            result = Character.compare(o2.charAt(i), o1.charAt(i));
         }
-        if (o1.contains("/") && o2.contains("/") && o1.substring(0, o1.indexOf("/")).equals(o2.substring(0, o2.indexOf("/")))) {
+        if (result == 0) {
             result = o1.compareTo(o2);
         }
         return result;
--- chapter_003/src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -38,7 +38,7 @@ public class DepartmentsTest {
     @Test
     public void whenSortDesc() {
         List<String> input = Arrays.asList("k1/sk1/ssk1", "k2/sk1");
-        List<String> expect = Arrays.asList("k2", "k2/sk1","k1", "k1/sk1", "k1/sk1/ssk1");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k1", "k1/sk1", "k1/sk1/ssk1");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
         assertThat(result, is(expect));
@@ -47,7 +47,7 @@ public class DepartmentsTest {
     @Test
     public void whenSortDesc2() {
         List<String> input = Arrays.asList("k1/sk1/ssk1", "k1/sk1/ssk2", "k2/sk1");
-        List<String> expect = Arrays.asList("k2", "k2/sk1","k1", "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2");
+        List<String> expect = Arrays.asList("k2", "k2/sk1", "k1", "k1/sk1", "k1/sk1/ssk1", "k1/sk1/ssk2");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
         assertThat(result, is(expect));
