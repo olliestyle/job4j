10909743abbd6f8a262497d2a631091f7d1c26ed
olliestyle <olliestyle1989@gmail.com>
2020-05-08 01:13:09 +0300

    4. Преобразование List в Map.[#222250]

--- chapter_004/src/test/java/ru/job4j/lambda/SchoolTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/SchoolTest.java
@@ -4,7 +4,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -21,6 +24,24 @@ public class SchoolTest {
                 new Student(60), new Student(70),
                 new Student(80), new Student(90),
                 new Student(100));
+    }
+
+    @Test
+    public void collectToMap() {
+        Map<Integer, Student> expected = new HashMap<>();
+        expected.put(0, new Student(0));
+        expected.put(10, new Student(10));
+        expected.put(20, new Student(20));
+        expected.put(30, new Student(30));
+        expected.put(40, new Student(40));
+        expected.put(50, new Student(50));
+        expected.put(60, new Student(60));
+        expected.put(70, new Student(70));
+        expected.put(80, new Student(80));
+        expected.put(90, new Student(90));
+        expected.put(100, new Student(100));
+        Map<Integer, Student> result = input.stream().distinct().collect(Collectors.toMap(Student::getScore, student -> student));
+        assertThat(result, is(expected));
     }
 
     @Test
