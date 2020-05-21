2ff723e94b43562adc70ce7bf7fcc58e2684c260
olliestyle <olliestyle1989@gmail.com>
2020-05-21 23:16:04 +0300

    1111

---
+++ chapter_004/src/main/java/ru/job4j/lambda/ListExample.java
@@ -0,0 +1,21 @@
+package ru.job4j.lambda;
+
+import java.util.List;
+
+public class ListExample {
+
+    public int j;
+    public ListExample listExample;
+
+    public static void main(String[] args) {
+
+//        int j;
+//        System.out.println(j);
+        List list;
+    }
+
+    public void hello() {
+        int i;
+        List list1;
+    }
+}
