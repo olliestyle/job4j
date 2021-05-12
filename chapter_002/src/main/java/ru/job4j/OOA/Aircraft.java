862f361500f84eaf64e8e24e77cd813373baba03
olliestyle <olliestyle1989@gmail.com>
2021-05-12 15:03:17 +0300

    4. final [#334073]

---
+++ chapter_002/src/main/java/ru/job4j/OOA/Aircraft.java
@@ -0,0 +1,7 @@
+package ru.job4j.OOA;
+
+public class Aircraft {
+    public void printModel() {
+        System.out.println("Метод выводит в консоль модель самолета.");
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/OOA/Airport.java
@@ -0,0 +1,20 @@
+package ru.job4j.OOA;
+
+public class Airport {
+    public static void main(String[] args) {
+        final Airbus airbus = new Airbus("A320");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+
+        final Airbus airbusA380 = new Airbus("A380");
+        System.out.println(airbusA380);
+        airbusA380.printModel();
+        airbusA380.printCountEngine();
+
+        airbus.setName("A380");
+        System.out.println(airbus);
+        airbus.printModel();
+        airbus.printCountEngine();
+    }
+}
